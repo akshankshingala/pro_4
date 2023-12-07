b2a60f104a3b96fa4cf46d6a9220ef805dc5107f
akshankshingala <150037897+akshankshingala@users.noreply.github.com>
2023-12-07 17:13:26 +0530

    Update PR4.1.c

--- PR4.1.c
+++ PR4.1.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
-
+/*
+Q.1 Write a Program to print the below pattern using nested for loop.
+41
+41 42
+41 42 43
+41 42 43 44
+41 42 43 44 45
+*/
+	
 main()
 {  int i,j,k;
      for(i=5;i>=1;i--){
