29013a8ac24cd409bb17f678a81b6fc960e2ec0b
akshankshingala <150037897+akshankshingala@users.noreply.github.com>
2023-12-07 17:15:48 +0530

    Update PR4.3.c

--- PR4.3.c
+++ PR4.3.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-
+/*
+Q.3 Write a Program to print the below pattern using nested for loop.
+            5
+         4 5
+      3 4 5
+   2 3 4 5
+1 2 3 4 5 */
 main()
 {  int i,j,k;
      for(i=5;i>=1;i--){
