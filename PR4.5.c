9794a7fbdc13a0923271ba2dd28ee961e78b2a5a
akshankshingala <150037897+akshankshingala@users.noreply.github.com>
2023-12-07 17:17:23 +0530

    Update PR4.5.c

--- PR4.5.c
+++ PR4.5.c
@@ -1,4 +1,13 @@
 #include<conio.h>
+/*
+    Q.5 Write a Program to print the below pattern using nested for loop.
+1                 1
+1 2             2 1
+1 2 3         3 2 1
+1 2 3 4     4 3 2 1
+1 2 3 4 5 5 4 3 2 1
+
+*/
 void main(){
 int i,j,k;
 for(i=1;i<=5;i++){
